f3147d58e980c8e3d120e165548db2222968e801
Sanchez-Guerrero <moises0968@outlook.com>
2021-04-13 17:13:45 -0500

    add sw

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = "cache_pokedex",
+  urlsToCache = [
+    "./",
+    "./styles/styles.css",
+    "./js/index.js",
+    "img/PikPng.com_pokeballs-png_1358145.png",
+    "https://github.com/Sanchez-Guerrero",
+  ];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache).then(() => self.skipWaiting());
+      })
+      .catch((err) => console.log("Failded register to cache", err))
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  const cacheWhitelist = [CACHE_NAME];
+
+  e.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      if (res) {
+        return res;
+      }
+      return fetch(e.request);
+    })
+  );
+});
